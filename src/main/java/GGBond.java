ebf4edde48c76bc9e2e3931719f3432fa6623f8e
GGBondxX <175328012+GGBondxX@users.noreply.github.com>
2026-01-24 13:05:32 +0800

    changed chatbot name,Increment level-0

---
+++ src/main/java/GGBond.java
@@ -0,0 +1,9 @@
+public class GGBond {
+    public static void main(String[] args) {
+        System.out.println("----------------------------------------------------");
+        System.out.println("Hello! I'm GGBond.\n"  + " What can I do for you?\n");
+        System.out.println("----------------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("----------------------------------------------------");
+    }
+}
