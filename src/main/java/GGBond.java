772853fc5c451bc0dd654994e37d980f6e0d88e8
GGBondxX <175328012+GGBondxX@users.noreply.github.com>
2026-01-29 03:10:36 +0800

    Mark as Done Completed. Increment Level-3

--- src/main/java/GGBond.java
+++ src/main/java/GGBond.java
@@ -4,33 +4,53 @@ import java.util.Scanner;
 public class GGBond {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int inputCounter = 0;
+        int taskNumber;
         String line;
 
         System.out.println("----------------------------------------------------");
         System.out.println("Hello! I'm GGBond.\n"  + " What can I do for you?\n");
         System.out.println("----------------------------------------------------");
         line = in.nextLine();
+        String[] words = line.split(" ");
 
-        while (!(line.equals("bye"))) {
-            if(line.equals("list")) {
+        while (!(words[0].equals("bye"))) {
+            if(words[0].equals("list")) {
                 int displayInputCount = 1;
                 System.out.println("----------------------------------------------------");
                 for (int i = 0; i < inputCounter; i++) {
-                    System.out.printf("%d. %s\n" , displayInputCount, list[i]);
+                    System.out.printf("%d.[%s] %s\n" , displayInputCount, list[i].getStatusIcon(), list[i].description);
                     displayInputCount++;
                 }
                 System.out.println("----------------------------------------------------");
             }
+            else if (words[0].equals("mark")) {
+                taskNumber =Integer.parseInt(words[1]) - 1;
+                list[taskNumber].setToDone();
+                System.out.println("----------------------------------------------------");
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.printf("[%s] %s\n",list[taskNumber].getStatusIcon(), list[taskNumber].description);
+                System.out.println("----------------------------------------------------");
+            }
+            else if (words[0].equals("unmark")) {
+                taskNumber =Integer.parseInt(words[1]) - 1;
+                list[taskNumber].setToUndone();
+                System.out.println("----------------------------------------------------");
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.printf("[%s] %s\n",list[taskNumber].getStatusIcon(), list[taskNumber].description);
+                System.out.println("----------------------------------------------------");
+            }
             else {
-                list[inputCounter] = line;
+                list[inputCounter] = new Task(line);
                 inputCounter++;
                 System.out.println("----------------------------------------------------");
                 System.out.printf("added: %s\n", line);
                 System.out.println("----------------------------------------------------");
             }
             line = in.nextLine();
+            words = line.split(" ");
+
         }
         System.out.println("----------------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void setToDone() {
+        this.isDone = true;
+    }
+
+    public void setToUndone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+}
