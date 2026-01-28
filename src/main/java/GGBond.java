f5e9e2bcfb95a13bb2f1180a83d557ab6122ab47
GGBondxX <175328012+GGBondxX@users.noreply.github.com>
2026-01-28 23:52:23 +0800

    Echo Completed. Increment Level-1

--- src/main/java/GGBond.java
+++ src/main/java/GGBond.java
@@ -1,7 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class GGBond {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String line;
         System.out.println("----------------------------------------------------");
         System.out.println("Hello! I'm GGBond.\n"  + " What can I do for you?\n");
+        line = in.nextLine();
+        String[] words = line.split(" ");
+        while (!(words[0].equals("bye"))) {
+            System.out.println(line);
+            line = in.nextLine();
+            words = line.split(" ");
+        }
         System.out.println("----------------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("----------------------------------------------------");
