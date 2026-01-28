31f26e0c8a485cadcf80ab9868978df975644aa1
GGBondxX <175328012+GGBondxX@users.noreply.github.com>
2026-01-29 00:20:11 +0800

    Add,List Completed. Increment Level-2

--- src/main/java/GGBond.java
+++ src/main/java/GGBond.java
@@ -4,15 +4,33 @@ import java.util.Scanner;
 public class GGBond {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String[] list = new String[100];
+        int inputCounter = 0;
         String line;
+
         System.out.println("----------------------------------------------------");
         System.out.println("Hello! I'm GGBond.\n"  + " What can I do for you?\n");
+        System.out.println("----------------------------------------------------");
         line = in.nextLine();
-        String[] words = line.split(" ");
-        while (!(words[0].equals("bye"))) {
-            System.out.println(line);
+
+        while (!(line.equals("bye"))) {
+            if(line.equals("list")) {
+                int displayInputCount = 1;
+                System.out.println("----------------------------------------------------");
+                for (int i = 0; i < inputCounter; i++) {
+                    System.out.printf("%d. %s\n" , displayInputCount, list[i]);
+                    displayInputCount++;
+                }
+                System.out.println("----------------------------------------------------");
+            }
+            else {
+                list[inputCounter] = line;
+                inputCounter++;
+                System.out.println("----------------------------------------------------");
+                System.out.printf("added: %s\n", line);
+                System.out.println("----------------------------------------------------");
+            }
             line = in.nextLine();
-            words = line.split(" ");
         }
         System.out.println("----------------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
