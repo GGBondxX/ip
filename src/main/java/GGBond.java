8f83cfc732f2639e2a0a32bbf82a2f890c47f2b9
GGBondxX <175328012+GGBondxX@users.noreply.github.com>
2026-02-03 17:40:56 +0800

    Completed ToDo, Event, Deadloine. Increment Level 4

--- src/main/java/GGBond.java
+++ src/main/java/GGBond.java
@@ -6,7 +6,7 @@ public class GGBond {
         Scanner in = new Scanner(System.in);
         Task[] list = new Task[100];
         int inputCounter = 0;
-        int taskNumber;
+        int taskNumber = 0;
         String line;
 
         System.out.println("----------------------------------------------------");
@@ -41,12 +41,18 @@ public class GGBond {
                 System.out.printf("%s\n",list[taskNumber].toString());
                 System.out.println("----------------------------------------------------");
             }
+            else if (words[0].equals("todo")) {
+                inputCounter = addToTaskList(list, words, inputCounter, TaskType.TODO);
+            }
+            else if (words[0].equals("deadline")) {
+                inputCounter = addToTaskList(list, words, inputCounter, TaskType.DEADLINES);
+            }
+            else if (words[0].equals("event")) {
+                inputCounter = addToTaskList(list, words, inputCounter, TaskType.EVENT);
+            }
             else {
-                list[inputCounter] = new Task(line);
-                inputCounter++;
-                System.out.println("----------------------------------------------------");
-                System.out.printf("added: %s\n", line);
-                System.out.println("----------------------------------------------------");
+                inputCounter = addToTaskList(list, words, inputCounter, TaskType.BASE);
+
             }
             line = in.nextLine();
             words = line.split(" ");
@@ -54,6 +60,70 @@ public class GGBond {
         }
         System.out.println("----------------------------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("----------------------------------------------------");
+    }
+
+    public static int addToTaskList(Task[] list, String[] words, int inputCounter, TaskType type) {
+        if (type == TaskType.BASE) {
+            String line = String.join(" ", words);
+            list[inputCounter] = new Task(line);
+            printAddMessage(list, inputCounter);
+            return ++inputCounter;
+        }
+
+        String description = "";
+        String by = "";
+        String from = "";
+        String to = "";
+        String currentSection = "desc";
+
+        for (int i = 1; i < words.length; i++) {
+            if(words[i].equals("/by")) {
+                currentSection = "by";
+            }
+            else if (words[i].equals("/from")) {
+                currentSection = "from";
+            }
+            else if (words[i].equals("/to")) {
+                currentSection = "to";
+            }
+            else {
+                switch (currentSection) {
+                case "desc":
+                    description += (description.isEmpty() ? "" : " ") + words[i];
+                    break;
+                case "by":
+                    by += (by.isEmpty() ? "" : " ") + words[i];
+                    break;
+                case "from":
+                    from += (from.isEmpty() ? "" : " ") + words[i];
+                    break;
+                case "to":
+                    to += (to.isEmpty() ? "" : " ") + words[i];
+                    break;
+                }
+            }
+        }
+        switch(type) {
+        case TODO :
+            list[inputCounter] = new Todo(description);
+            break;
+        case DEADLINES :
+            list[inputCounter] = new Deadlines(description, by);
+            break;
+        case EVENT :
+            list[inputCounter] = new Events(description, from, to);
+            break;
+        }
+        printAddMessage(list, inputCounter);
+        return ++inputCounter;
+    }
+
+    public static void printAddMessage(Task[] list, int inputCounter) {
+        System.out.println("----------------------------------------------------");
+        System.out.println("Got it. I've added this task:");
+        System.out.printf("%s\n", list[inputCounter].toString());
+        System.out.printf("Now you have %d tasks in the list\n", inputCounter+1);
         System.out.println("----------------------------------------------------");
     }
 }
