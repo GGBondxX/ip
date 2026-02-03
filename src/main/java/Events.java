24cfdd0d7ce4c960646feae7f26952ecb48bd352
GGBondxX <175328012+GGBondxX@users.noreply.github.com>
2026-02-03 16:16:40 +0800

    Added the three child classes for Task

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -1,2 +1,16 @@
-public class Events {
+public class Events extends Task{
+
+    protected String from;
+    protected String to;
+
+    public Events(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E}" + super.toString() + " (from: " + from + " to: " + to + ")";
+    }
 }
